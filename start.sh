f2551a50fc64c55b8c7eedeecc263895ff407f9a
zzq <zzq@qr.com>
2018-07-24 17:24:39 +0800

    修改主程序和子程序的名字

--- start.sh
+++ start.sh
@@ -16,6 +16,6 @@ cd ~/dss_vt322
 chmod 777 51
 chmod 777 VT322_main_new
 
-echo "ubuntu" | nohup sudo -S ./51 > /dev/null 2>&1 &
+echo "ubuntu" | nohup sudo -S ./VT322_sub > /dev/null 2>&1 &
 #sleep 0.1
-echo "ubuntu" | nohup sudo -S ./VT322_main_new > /dev/null 2>&1 &
+echo "ubuntu" | nohup sudo -S ./VT322_main > /dev/null 2>&1 &
